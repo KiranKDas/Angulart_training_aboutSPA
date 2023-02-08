a54fb69c041d670c1d413b86fb13dde558342d46
KiranKDas <85393415+KiranKDas@users.noreply.github.com>
2023-02-08 10:35:00 +0530

    Final commit

---
+++ AngularSPA/src/app/Components/vision/vision.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vision',
+  templateUrl: './vision.component.html',
+  styleUrls: ['./vision.component.css']
+})
+export class VisionComponent {
+
+}
+ 
